6712bb2f540eba1140409bdc550355d034f50efb
ThanhHuynh007 <neil20012003@gmail.com>
2024-11-09 12:23:30 +0700

    no message

--- src/main/java/com/example/demo2/model/Company.java
+++ src/main/java/com/example/demo2/model/Company.java
@@ -2,6 +2,8 @@
 package com.example.demo2.model;
 
 import jakarta.persistence.*;
+import lombok.Setter;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,10 +11,12 @@ import java.util.List;
 @Table(name = "COMPANY")
 public class Company {
 
+    @Setter
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Setter
     @Column(name = "name")
     private String name;
 
@@ -28,9 +32,8 @@ public class Company {
 
     // Getters and setters for id and name
     public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
+
     public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
 
     public List<UserDemo> getUsers() { return users; }
 
